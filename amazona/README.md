3e9edb59da25dd5313fa358cffc772be17041e64
Venkateshkumar1 <kishorkani4321@gmail.com>
2023-02-28 16:17:46 +0530

    featured-products

--- amazona/README.md
+++ amazona/README.md
@@ -4,3 +4,8 @@
 
 1. Create React App
 2. Create Git Repository
+3. List Products
+   1. create products array
+   2. add products image
+   3. render products
+   4. style products
---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirts',
+            slug: 'nike-slim-shirt',
+            catagory: 'shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: '10',
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            discription: 'high quality shirts',
+        },
+
+        {
+            name: 'Adidas Fit Shirts',
+            slug: 'adidas-fit-shirt',
+            catagory: 'shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: '20',
+            brand:'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            discription: 'high quality shirts',
+        },
+
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            catagory: 'pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInStock: 15,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            discription: 'high quality shirts',
+        },
+
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas fit pant',
+            catagory: 'Pants',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInStock: 5,
+            brand:'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            discription: 'high quality shirts',
+        },
+    ],
+};
+
+export default data;
